0c4a9c203f26d71332c14c4e17f1faeaaeac6ccb
baptistemcn <baptistemcn@icloud.com>
2023-05-18 08:55:09 +0200

    refactor(wrapper): wrapping all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import { Wrapper } from "./components";
 
 function App() {
   return (
     <>
-      <Wrapper>
-        <h1>Baptiste Marcon</h1>
-      </Wrapper>
+      <h1>Baptiste Marcon</h1>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
+import { Wrapper } from "./components/Wrapper.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Wrapper>
+      <App />
+    </Wrapper>
   </React.StrictMode>,
 );
