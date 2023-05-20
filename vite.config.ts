abd91126a7dfe6ffc151d9709877bf00efa632c8
baptistemcn <baptistemcn@icloud.com>
2023-05-20 20:05:46 +0200

    chore(vite): config entry file on env

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 
+const dirname =
+  process.env.NODE_ENV === "production"
+    ? resolve(__dirname, "200.html")
+    : resolve(__dirname, "index.html");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
@@ -9,7 +14,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "200.html"),
+        main: dirname,
       },
     },
   },
