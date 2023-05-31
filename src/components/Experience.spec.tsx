dac96dec61d21bd4d00abf91c327cf3a845c09e8
baptistemcn <baptistemcn@icloud.com>
2023-05-31 21:27:35 +0200

    test(experience): full coverage

--- src/components/Experience.spec.tsx
+++ src/components/Experience.spec.tsx
@@ -17,4 +17,51 @@ describe("Experience Component", () => {
   it("should render", () => {
     render(<Experience experiences={mockExperiences} />);
   });
+
+  it("should renders experiences", () => {
+    const { container } = render(<Experience experiences={mockExperiences} />);
+    const experienceElements = container.querySelectorAll(".experience");
+    expect(experienceElements.length).toBe(mockExperiences.length);
+  });
+
+  it("should render experiences details", () => {
+    const { getByText } = render(<Experience experiences={mockExperiences} />);
+    const experienceTitles = mockExperiences.map(
+      (experience) => experience.title,
+    );
+    const experiencePresentations = mockExperiences.map(
+      (experience) => experience.presentation,
+    );
+
+    experienceTitles.forEach((title) => {
+      expect(getByText(title)).toBeInTheDocument();
+    });
+
+    experiencePresentations.forEach((presentation) => {
+      expect(getByText(presentation)).toBeInTheDocument();
+    });
+  });
+
+  it("should render technologies for each experiences", () => {
+    const { container } = render(<Experience experiences={mockExperiences} />);
+    const technologyList = container.querySelectorAll(".experience ul");
+
+    expect(technologyList.length).toBe(mockExperiences.length);
+
+    mockExperiences.forEach((experience, index) => {
+      const technologyItems = technologyList[index].querySelectorAll("li");
+      expect(technologyItems.length).toBe(experience.technologies.length);
+    });
+  });
+
+  it("should render technologies name", () => {
+    const { getByText } = render(<Experience experiences={mockExperiences} />);
+    const technologyNames = mockExperiences
+      .flatMap((experience) => experience.technologies)
+      .map((technology) => technology.name);
+
+    technologyNames.forEach((name) => {
+      expect(getByText(name)).toBeInTheDocument();
+    });
+  });
 });
