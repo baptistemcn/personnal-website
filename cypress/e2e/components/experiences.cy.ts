069d77444bf7bc89446af35198006c4fa71d76f3
baptistemcn <baptistemcn@icloud.com>
2023-06-08 21:07:14 +0200

    test(e2e): experience component

---
+++ cypress/e2e/components/experiences.cy.ts
@@ -0,0 +1,29 @@
+describe("experiences component", () => {
+  it("should render", () => {
+    cy.visit("/en/about");
+  });
+
+  it("should render experience card", () => {
+    cy.visit("/en/about");
+
+    cy.get(".about-experience > div").should("be.visible");
+  });
+
+  it("should render a title in experience card", () => {
+    cy.visit("/en/about");
+
+    cy.get(".about-experience > div > div > h3").should("be.visible");
+  });
+
+  it("should render a presentation text in experience", () => {
+    cy.visit("/en/about");
+
+    cy.get(".about-experience > div > div > p").should("be.visible");
+  });
+
+  it("should render at least one experience in experience card", () => {
+    cy.visit("/en/about");
+
+    cy.get(".about-experience > div > div > ul > li").should("be.visible");
+  });
+});
