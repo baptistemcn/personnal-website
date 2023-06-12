80ae44b93db589b757f8448e139e589740f4c71a
baptistemcn <baptistemcn@icloud.com>
2023-06-12 23:04:33 +0200

    feat(lang): language switcher

--- src/components/Header.spec.tsx
+++ src/components/Header.spec.tsx
@@ -2,7 +2,15 @@ import { fireEvent, render } from "@testing-library/react";
 import { Header } from "./Header";
 import { BrowserRouter } from "react-router-dom";
 
-describe("Header Cmponent", () => {
+jest.mock("react-i18next", () => ({
+  ...jest.requireActual("react-i18next"),
+  useTranslation: () => ({
+    i18n: { language: "en", changeLanguage: jest.fn() },
+    t: (key: unknown) => key,
+  }),
+}));
+
+describe("Header Component", () => {
   const headerMockProps = [
     { name: "Home", url: "/" },
     { name: "About", url: "/about" },
@@ -47,5 +55,39 @@ describe("Header Cmponent", () => {
 
     expect(firstNavItem.parentElement).not.toHaveClass("active");
     expect(secondNavItem.parentElement).toHaveClass("active");
+  });
+
+  it("should render a flag in language button", () => {
+    const flagMock =
+      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAbUlEQVR4nO3YwQ3AIAxD0SyGMgPDshUSYoL01gHoiep9yQMgILEdAQDAH8g+6kRz7U+qFkdygHQD2xPyidMU2sboi0W2bOJiJZKZ29xosdNdoCmJrImUQ6ifWolQq5Ria2nmSrWYt5S7AADEhTw3HH3PGGqzVQAAAABJRU5ErkJggg==";
+
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Header headers={headerMockProps} />
+      </BrowserRouter>,
+    );
+
+    const languageButton = getByTestId("lang-switch");
+    expect(languageButton).toBeInTheDocument();
+
+    const flagComponent = languageButton.querySelector("img");
+
+    expect(flagComponent?.src).toEqual(flagMock);
+  });
+
+  it("should change language on language button click", () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Header headers={headerMockProps} />
+      </BrowserRouter>,
+    );
+
+    const languageButton = getByTestId("lang-switch");
+
+    expect(languageButton).toBeInTheDocument();
+
+    const clicked = fireEvent.click(languageButton);
+
+    expect(clicked).toBeTruthy();
   });
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+import { Flag } from "./Flag";
 
 import "./header.css";
-import { useState } from "react";
+import { FRENCH_FLAG, US_FLAG } from "../assets/db";
 
 interface HeaderProps {
   headers: Array<LinksProps>;
@@ -14,26 +18,44 @@ interface LinksProps {
 
 export const Header = ({ headers }: HeaderProps) => {
   const [activeNavItem, setActiveNavItem] = useState("");
+  const { i18n } = useTranslation();
+
+  const language = i18n.language;
 
   const handleNavItemClick = (item: string) => {
     setActiveNavItem(item);
   };
 
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  const handleChangeLanguage = () => {
+    /* istanbul ignore next */
+    const lang = language === "en" ? "fr" : "en";
+    changeLanguage(lang);
+  };
+
+  /* istanbul ignore next */
+  const handleFlag = language === "fr" ? FRENCH_FLAG : US_FLAG;
+
   return (
     <header>
       <nav>
+        <button onClick={handleChangeLanguage} data-testid="lang-switch">
+          {language && <Flag nation={handleFlag} />}
+        </button>
+
         <ul>
-          {headers.map((item, index) => {
-            return (
-              <li
-                key={index}
-                onClick={() => handleNavItemClick(item.name)}
-                className={activeNavItem === item.name ? "active" : ""}
-              >
-                <Link to={item.url}>{item.name}</Link>
-              </li>
-            );
-          })}
+          {headers.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => handleNavItemClick(item.name)}
+              className={activeNavItem === item.name ? "active" : ""}
+            >
+              <Link to={item.url}>{item.name}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
