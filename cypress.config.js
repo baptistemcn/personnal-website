9ce845011d2f91e541c92d2ed0f7b38277b55c0d
baptistemcn <baptistemcn@icloud.com>
2023-06-13 19:03:42 +0200

    ci(github): cypress e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-undef */
 import { defineConfig } from "cypress";
+
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5173"
+    : "https://baptistemcn.surge.sh/en/";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    // baseUrl: "http://localhost:5173",
+    baseUrl: url,
+
     video: false,
     chromeWebSecurity: false,
   },
