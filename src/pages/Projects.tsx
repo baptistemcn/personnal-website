8fe959f58b8c192c078df39a9ebdb89b7580f192
baptistemcn <baptistemcn@icloud.com>
2023-06-15 23:31:27 +0200

    feat(project): presentation design

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,14 @@
 import { useTranslation } from "react-i18next";
 
+import "./projects.css";
+
 export const Projects = () => {
   const { t } = useTranslation();
 
-  return <h1>{t("projects.title")}</h1>;
+  return (
+    <div className="project">
+      <h1>{t("projects.title")}</h1>
+      <p>...</p>
+    </div>
+  );
 };
