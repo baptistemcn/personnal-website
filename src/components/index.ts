015c60f8fc4478921e2ca61cf841a25720b7429c
baptistemcn <baptistemcn@icloud.com>
2023-06-18 12:09:23 +0200

    refactor(footer): folder structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { Card } from "./card/Card";
 export { Certificate } from "./certificate/Certificate";
 export { Experience } from "./experience/Experience";
-export { Footer } from "./Footer";
+export { Footer } from "./footer/Footer";
 export { Header } from "./Header";
 export { Wrapper } from "./Wrapper";
