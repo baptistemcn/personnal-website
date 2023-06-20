228d37a4b297fe9bf061294e3039d42985a64bdb
baptistemcn <baptistemcn@icloud.com>
2023-06-20 06:55:32 +0200

    test(experience): duration experience coverage

--- src/components/experience/Experience.spec.tsx
+++ src/components/experience/Experience.spec.tsx
@@ -12,7 +12,7 @@ describe("Experience Component", () => {
     {
       title: "Experience 2",
       presentation: "Presentation 2",
-      duration: "Janv. 2022",
+      duration: "Janv. 2023",
       technologies: [{ name: "Tech 3" }, { name: "Tech 4" }],
     },
   ];
@@ -22,30 +22,51 @@ describe("Experience Component", () => {
 
   it("should renders experiences", () => {
     const { container } = render(<Experience experiences={mockExperiences} />);
+
     const experienceElements = container.querySelectorAll(".experience");
+
     expect(experienceElements.length).toBe(mockExperiences.length);
   });
 
   it("should render experiences details", () => {
     const { getByText } = render(<Experience experiences={mockExperiences} />);
+
     const experienceTitles = mockExperiences.map(
       (experience) => experience.title,
-    );
-    const experiencePresentations = mockExperiences.map(
-      (experience) => experience.presentation,
     );
 
     experienceTitles.forEach((title) => {
       expect(getByText(title)).toBeInTheDocument();
     });
+  });
+
+  it("should render experiences presentation", () => {
+    const { getByText } = render(<Experience experiences={mockExperiences} />);
+
+    const experiencePresentations = mockExperiences.map(
+      (experience) => experience.presentation,
+    );
 
     experiencePresentations.forEach((presentation) => {
       expect(getByText(presentation)).toBeInTheDocument();
     });
   });
 
+  it("should render experiences duration", () => {
+    const { getByText } = render(<Experience experiences={mockExperiences} />);
+
+    const experienceDuration = mockExperiences.map(
+      (experience) => experience.duration,
+    );
+
+    experienceDuration.forEach((duration) => {
+      expect(getByText(duration)).toBeInTheDocument();
+    });
+  });
+
   it("should render technologies for each experiences", () => {
     const { container } = render(<Experience experiences={mockExperiences} />);
+
     const technologyList = container.querySelectorAll(".experience ul");
 
     expect(technologyList.length).toBe(mockExperiences.length);
