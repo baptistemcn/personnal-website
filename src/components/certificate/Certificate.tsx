7075a46fd982ac903fba9292d4da8f9c60fd646e
baptistemcn <baptistemcn@icloud.com>
2023-06-21 22:33:47 +0200

    fix(certificate): adjust link role

--- src/components/certificate/Certificate.tsx
+++ src/components/certificate/Certificate.tsx
@@ -8,7 +8,9 @@ export const Certificate = ({ certificates }: CertificatesProps) => {
         return (
           <article className="certificate" key={index}>
             <h2>{cert.name}</h2>
-            <a href={cert.link}>{cert.link}</a>
+            <a href={cert.link} target="_blank" rel="noopener">
+              {cert.link}
+            </a>
             {cert.download && (
               <a href={cert.download} target="_blank" rel="noopener" download>
                 <button>{cert.label}</button>
