515e319edab42562a48bca5ace046d7a34893606
Baptiste Marcon <baptistemcn@icloud.com>
2023-06-25 09:00:15 +0200

    style(project): adjust grid system

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -2,11 +2,15 @@ import "./project.css";
 
 export const Project = () => {
   return (
-    <article className="project">
-      <h2>First Project</h2>
-      <p>This is my project</p>
-      <a href="">Github</a>
-      <a href="">Hosted</a>
-    </article>
+    <>
+      <article className="project">
+        <h2>First Project</h2>
+        <p>This is my project</p>
+        <div>
+          <a href="">Github</a>
+          <a href="">Hosted</a>
+        </div>
+      </article>
+    </>
   );
 };
