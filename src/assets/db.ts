001feab3f0c03f57a447318c45e487b6ae52bdee
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-02 12:34:51 +0200

    fix(experience): adjust db file

--- src/assets/db.ts
+++ src/assets/db.ts
@@ -16,7 +16,7 @@ export const ICONS = [
 
 const EXPERIENCES_FR = [
   {
-    title: "Full-Stack Developper - NEHS Digital",
+    name: "Full-Stack Developper - NEHS Digital",
     presentation:
       "Dans mon rôle actuel, je me concentre sur le développement de nouvelles fonctionnalités, la création d'intégrations de design attrayantes et la gestion du code hérité, contribuant ainsi à l'évolution et à l'amélioration continues de nos produits.",
     duration: "Janv. 2022",
@@ -51,7 +51,7 @@ const EXPERIENCES_FR = [
 
 const EXPERIENCES_EN = [
   {
-    title: "Full-Stack Developer - NEHS Digital",
+    name: "Full-Stack Developer - NEHS Digital",
     presentation:
       "In my current role, I focus on developing new features, creating appealing design integrations, and managing legacy code, thereby contributing to the continuous evolution and improvement of our products.",
     duration: "Janv. 2022",
