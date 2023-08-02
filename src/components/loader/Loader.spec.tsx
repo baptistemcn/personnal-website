e76c47dabef8181485e338fc559df1bd7e9c194d
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-02 12:22:12 +0200

    feat(loader): add loader style

---
+++ src/components/loader/Loader.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { Loader } from "..";
+
+describe("Loader component", () => {
+  it("should render", () => {
+    render(<Loader />);
+  });
+});
--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,3 +1,12 @@
+import "./loader.css";
+
 export const Loader = () => {
-  return <p>Loading...</p>;
+  return (
+    <div className="loader">
+      <div className="loader-bar bar1"></div>
+      <div className="loader-bar bar2"></div>
+      <div className="loader-bar bar3"></div>
+      <div className="loader-bar bar4"></div>
+    </div>
+  );
 };
