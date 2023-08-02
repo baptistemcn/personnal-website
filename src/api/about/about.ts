71fc3d7f24cbd6709ab784d05fea0480c4173aa4
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-02 12:44:05 +0200

    feat(about): add api request

---
+++ src/api/about/about.ts
@@ -0,0 +1,11 @@
+import { fetch } from "cross-fetch";
+
+const API_URL = "https://baptiste-marcon-api.adaptable.app/";
+
+export async function getAbout(lang: string) {
+  const response = await fetch(`${API_URL}/about?language=${lang}`, {
+    mode: "cors",
+    method: "GET",
+  });
+  return await response.json();
+}
---
+++ src/api/index.ts
@@ -0,0 +1,1 @@
+export { getAbout } from "./about/about";
