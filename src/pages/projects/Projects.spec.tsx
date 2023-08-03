54907fbaf853efad950a50b450c1d2d1cfbb2357
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-03 20:19:21 +0200

    feat(projects): add api calls & loading / error state

--- src/pages/projects/Projects.spec.tsx
+++ src/pages/projects/Projects.spec.tsx
@@ -1,5 +1,7 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { Projects } from "./Projects";
+
+import * as api from "../../api";
 
 jest.mock("react-i18next", () => ({
   useTranslation: () => ({
@@ -11,6 +13,25 @@ jest.mock("react-i18next", () => ({
 }));
 
 describe("Projects Page", () => {
+  const mockProjectProps = [
+    {
+      title: "test title 1",
+      description: "test presentation 1",
+      host: {
+        name: "test hosted 1",
+        url: "test hosted 1",
+      },
+    },
+    {
+      title: "test title 2",
+      description: "test presentation 2",
+      host: {
+        name: "test hosted 2",
+        url: "test hosted url 2",
+      },
+    },
+  ];
+
   it("should render", () => {
     render(<Projects />);
   });
@@ -21,5 +42,59 @@ describe("Projects Page", () => {
     const titleElement = getByText("projects.title");
 
     expect(titleElement).toBeInTheDocument();
+  });
+
+  it("should render project component", () => {
+    const { getByTestId } = render(<Projects />);
+
+    const projectElement = getByTestId("projects");
+
+    expect(projectElement).toBeTruthy();
+    expect(projectElement).toBeInTheDocument();
+  });
+
+  it("should render project component with data", async () => {
+    jest.spyOn(api, "getProjects").mockResolvedValue(mockProjectProps);
+
+    const { getByTestId, getByText } = render(<Projects />);
+
+    const component = getByTestId("projects");
+    const loader = getByTestId("loader");
+
+    expect(component).toBeTruthy();
+    expect(component).toBeInTheDocument();
+
+    expect(loader).toBeTruthy();
+    expect(loader).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(loader).not.toBeInTheDocument();
+    });
+
+    expect(getByText("test title 1")).toBeInTheDocument();
+    expect(getByText("test title 2")).toBeInTheDocument();
+  });
+
+  it("should render project component with data", async () => {
+    jest
+      .spyOn(api, "getProjects")
+      .mockRejectedValue(new Error("Failed to fetch data"));
+
+    const { getByTestId, getByText } = render(<Projects />);
+
+    const component = getByTestId("projects");
+    const loader = getByTestId("loader");
+
+    expect(component).toBeTruthy();
+    expect(component).toBeInTheDocument();
+
+    expect(loader).toBeTruthy();
+    expect(loader).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(loader).not.toBeInTheDocument();
+    });
+
+    expect(getByText("Error !")).toBeInTheDocument();
   });
 });
--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import { Project } from "../../components";
+import { getProjects } from "../../api";
+import { Loader, Project } from "../../components";
+import { ProjectItem } from "../../types";
 
 import "./projects.css";
-import { PROJECTS } from "../../assets/db";
 
 export const Projects = () => {
+  const [project, SetProject] = useState<Array<ProjectItem>>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   const { t } = useTranslation();
+
+  useEffect(() => {
+    getProjects()
+      .then((data) => {
+        SetProject(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
 
   return (
     <div className="projects">
       <h1>{t("projects.title")}</h1>
-      <div>
-        <Project project={PROJECTS} />
+      <div data-testid="projects">
+        {loading && <Loader />}
+        {error && <p>Error !</p>}
+        <Project project={project} />
       </div>
     </div>
   );
