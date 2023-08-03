4b2d9d49bae8b62a4d2bedd855d5f390d599c0ce
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-03 21:34:27 +0200

    chore(render): adjust build config

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 import sitemap from "vite-plugin-sitemap";
 import robotsTxt from "./vite-plugin-robots-txt";
 
-const dirname =
-  process.env.NODE_ENV === "production"
-    ? resolve(__dirname, "200.html")
-    : resolve(__dirname, "index.html");
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
@@ -23,7 +18,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: dirname,
+        main: resolve(__dirname, "index.html"),
       },
     },
   },
