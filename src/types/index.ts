bf3b483c40436dfcb576436344c7b9f8d0ab7f30
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-03 20:13:37 +0200

    fix(project): add missing import

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,7 +7,7 @@ export type { ExperienceItem, ExperiencesProps } from "./experiences.interface";
 export type { HeaderProps } from "./header.interface";
 export type { IconsProps } from "./icons.interface";
 export type { FlagProps } from "./flag.interface";
-export type { ProjectProps } from "./project.interface";
+export type { ProjectItem, ProjectProps } from "./project.interface";
 export type {
   TechnologiesItem,
   TechnologiesProps,
